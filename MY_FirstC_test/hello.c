cd47439eaa94c0d3a0673f8a0e0083c65bbb08d0
dongxiaoyuu <2387230197@qq.com>
2021-11-10 16:06:55 +0800

    改为比较大小函数

--- MY_FirstC_test/hello.c
+++ MY_FirstC_test/hello.c
@@ -1,5 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+int Max(int a, int b) {
+	if (a > b)
+		return a;
+	return b;
+}
+
+
 int main() {
-	printf("hello world again!");
+	int a;
+	int b;
+	printf("请输入两个数：\n");
+	scanf("%d %d", &a,&b);
+	printf("比较大的数是：%d", Max(a, b));
 	return 0;
 }
